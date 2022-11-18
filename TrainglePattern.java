cd3fb53c4024f1c5734d72743dab5902c44225c7
1ms18cs422 <88642706+1ms18cs422@users.noreply.github.com>
2022-11-18 09:01:28 +0530

    Create TrainglePattern.java

---
+++ TrainglePattern.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Trainglepattern{
+public static void main (String a[])
+{
+Scanner sc= new Scanner(System.in);
+System.out.println("Input number of rows");
+int n=sc.nextInt();
+for(int i=0;i<10;i++){
+for(int j=1;j<=i ;j++){
+System.out.print(j+"");
+}
+System.out.println();
+}
+}
+}
