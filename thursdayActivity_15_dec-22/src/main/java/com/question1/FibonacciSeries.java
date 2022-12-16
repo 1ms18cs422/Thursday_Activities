b795a67d431740935816b892bd15f7342e1dc8dc
1ms18cs422 <88642706+1ms18cs422@users.noreply.github.com>
2022-12-16 09:35:37 +0530

    Update FibonacciSeries.java

--- thursdayActivity_15_dec-22/src/main/java/com/question1/FibonacciSeries.java
+++ thursdayActivity_15_dec-22/src/main/java/com/question1/FibonacciSeries.java
@@ -1,17 +1,18 @@
 package com.question1;
 
 public class FibonacciSeries {
-	int fibonacciSeries(int n) {
+	int fibonacciSeries(int number) {
 		int num1=0;
 		int num2=1;
+                Int result=0;
 		
-		for(int i=2;i<=10;i++) {
+		for(int i=2;i<=number;i++) {
 			num1=num2;
-			num2=n;
-			n=num1+num2;
+			num2=result;
+			result=num1+num2;
 		}
 			
-		return n;
+		return result;
 	}
 
 }
