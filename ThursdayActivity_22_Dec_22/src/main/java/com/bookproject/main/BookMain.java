b55c96b16922aa9ab928add1402b70ab31de4cdf
1ms18cs422 <88642706+1ms18cs422@users.noreply.github.com>
2022-12-23 12:38:45 +0530

    Update BookMain.java

--- ThursdayActivity_22_Dec_22/src/main/java/com/bookproject/main/BookMain.java
+++ ThursdayActivity_22_Dec_22/src/main/java/com/bookproject/main/BookMain.java
@@ -14,7 +14,7 @@ public class BookMain {
 		bdao.createBookTable();
 		System.out.println("inserting into book table");
 Book b1=new Book(01,"Java prograBookmming","balaguruswamy","features of java,exceptions,threads",450,"https://java;");
- Book b2=new Book(02,"Puzzels","satish","logical puzzels,number puzzels",450,"https://puzzels;");
+ Book b2=new Book(02,"C Progtramming","satish","logical puzzels,number puzzels",450,"https://puzzels;");
 Book  b3=new Book(03,"English","Marry com","Grammer,poems,lessons",350,"https://English");
 bdao.insertBook(b1);
 bdao.insertBook(b2);
